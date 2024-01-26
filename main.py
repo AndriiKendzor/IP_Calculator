1dfaa9f4081856b00b8e24024bdba0cb1143710a
AndriiKendzor <kendzorkv@gmail.com>
2024-01-27 00:46:44 +0100

    step 3-4 +

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask
 import re
 import math
 
-#Перевірка IP на цифри і точки
+
+
 '''
+#Перевірка IP на цифри і точки
 def validate_ip_address(ip):
     ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
 
@@ -13,29 +15,22 @@ def validate_ip_address(ip):
         print("IP-адреса не відповідає очікуваному формату.")
 '''
 
+
 #Перевірка та визначення класи IP
 def get_ip_class(ip):
-    ip_pattern = re.compile(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$')
-    match = ip_pattern.match(ip)
+    ip_pattern = re.compile(r'^(\d{1,3})\.')
+    first_octet = int(ip_pattern.search(ip).group(1))
 
-    if match:
-        octet1 = int(match.group(1))
-        octet2 = int(match.group(2))
-        octet3 = int(match.group(3))
-        octet4 = int(match.group(4))
-
-        if 1 <= octet1 <= 127 and octet4==0 and octet3==0 and octet2==0: #не знаю чи нам потрібна ця умова - треба перевірити !!!!!
-            return 'A'
-        elif 128 <= octet1 <= 191 and octet4==0 and octet3==0: #!!!!
-            return 'B'
-        elif 192 <= octet1 <= 223 and octet4==0: #!!!!
-            return 'C'
-        elif 224 <= octet1 <= 239:
-            return 'D'
-        elif 240 <= octet1 <= 255:
-            return 'E'
-        else:
-            return '-'
+    if 1 <= first_octet <= 127:
+        return 'A'
+    elif 128 <= first_octet <= 191:
+        return 'B'
+    elif 192 <= first_octet <= 223:
+        return 'C'
+    elif 224 <= first_octet <= 239:
+        return 'D'
+    elif 240 <= first_octet <= 255:
+        return 'E'
     else:
         return '-'
 
@@ -49,37 +44,43 @@ def find_power_of_two(host_count):
         return "Введене число має бути більше 0."
 
 
-
 #Перевірка хоста класи C
 def validate_host_C(host_count):
     if host_count>254:
         print("Не вірна кількість хостів для ip класи C")
 
 
-
 #Шукажмо класу С
+# Поиск для класса C
 def find_c(ip, pow2):
-
     ip_pattern = re.compile(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$')
     match = ip_pattern.match(ip)
     last_number_ip = int(match.group(4))
 
-    #N,H1,Host,R;
-    licznik_podsieci=0;
+    licznik_podsieci = 0
+    last_subnet_printed = False
 
-    while(last_number_ip < 256):
-        licznik_podsieci+=1
+    first_three_octets = f"{match.group(1)}.{match.group(2)}.{match.group(3)}"
+
+    while last_number_ip < 256:
+        licznik_podsieci += 1
 
         N = last_number_ip
-        H1 = N+1
+        H1 = N + 1
 
         last_number_ip = last_number_ip + pow2
 
-        H_ost = last_number_ip-2
-        R = last_number_ip-1
+        H_ost = last_number_ip - 2
+        R = last_number_ip - 1
 
-        print(f"{licznik_podsieci}p:")
-        print(f"N: {N}; 1H: {H1}; ostH: {H_ost}; R:{R};")
+        if last_number_ip >= 256:
+            if not last_subnet_printed:
+                print(f"ost. {licznik_podsieci}p:")
+                last_subnet_printed = True
+            print(f"ost. N: {first_three_octets}.{N}; 1H: {first_three_octets}.{H1}; ostH: {first_three_octets}.{H_ost}; R:{first_three_octets}.{R};")
+        else:
+            print(f"{licznik_podsieci}p:")
+            print(f"N: {first_three_octets}.{N}; 1H: {first_three_octets}.{H1}; ostH: {first_three_octets}.{H_ost}; R:{first_three_octets}.{R};")
 
 
 '''
@@ -108,24 +109,24 @@ print(f"\nIP-адреса належить до класи {ip_class}")
 
 
 #Вибір відповідної функції
-if ip_class=='C':
+if ip_class =='C':
     print("Маска за замовчуванням 255.255.255.0\n")
     #Перевіряємо
     validate_host_C(host_count)
     # Обчислити класу С
     find_c(ip_address, 2**power_of_two)
 
-elif ip_class=='B':
+elif ip_class == 'B':
     print("Маска за замовчуванням 255.255.0.0")
     # Обчислити класу B
     pass
-elif ip_class=='A':
+elif ip_class == 'A':
     print("Маска за замовчуванням 255.0.0.0")
     # Обчислити класу A
     pass
-elif ip_class=='D':
+elif ip_class == 'D':
     print("Даний IP належить до зарезервованої класи D")
-elif ip_class=='E':
+elif ip_class == 'E':
     print("Даний IP належить до зарезервованої класи E")
 else:
     print("Не валідний формат IP")
