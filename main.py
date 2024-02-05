22a5e17b79da3b1c28c9ae843995b5460293a9b4
Xinexz <yogurt2288@gmail.com>
2024-02-05 05:21:16 +0100

    *

--- main.py
+++ main.py
@@ -48,6 +48,9 @@ def find_power_of_two(host_count):
 def validate_host_C(host_count):
     if host_count>254:
         print("Не вірна кількість хостів для ip класи C")
+        return False
+    else:
+        return True
 
 
 #Шукажмо класу С
@@ -59,6 +62,8 @@ def find_c(ip, pow2):
 
     licznik_podsieci = 0
     last_subnet_printed = False
+
+    first_three_octets = f"{match.group(1)}.{match.group(2)}.{match.group(3)}"
 
     while last_number_ip < 256:
         licznik_podsieci += 1
@@ -73,12 +78,83 @@ def find_c(ip, pow2):
 
         if last_number_ip >= 256:
             if not last_subnet_printed:
-                print(f"ost. {licznik_podsieci}p:")
+                print(f"❗ ost. {licznik_podsieci}p:")
                 last_subnet_printed = True
-            print(f"ost. N: {N}; 1H: {H1}; ostH: {H_ost}; R:{R};")
+            print(f"ost. N: {first_three_octets}.{N}; 1H: {first_three_octets}.{H1}; ostH: {first_three_octets}.{H_ost}; R:{first_three_octets}.{R};")
         else:
             print(f"{licznik_podsieci}p:")
-            print(f"N: {N}; 1H: {H1}; ostH: {H_ost}; R:{R};")
+            print(f"N: {first_three_octets}.{N}; 1H: {first_three_octets}.{H1}; ostH: {first_three_octets}.{H_ost}; R:{first_three_octets}.{R};")
+
+
+#Перевірка хоста класи B
+def validate_host_B(host_count):
+    if host_count > 65534:
+        print("Не вірна кількість хостів для ip класи B")
+        return False
+    else:
+        return True
+
+
+#Шукажмо класу B
+# Поиск для класса B
+def find_B(ip, pow2):
+    ip_pattern = re.compile(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$')
+    match = ip_pattern.match(ip)
+
+    last_number_ip = int(match.group(4))
+    thrid_number_ip = int(match.group(3))
+
+    licznik_podsieci = 0
+    last_subnet_printed = False
+
+    #first_three_octets = f"{match.group(1)}.{match.group(2)}.{match.group(3)}"
+
+    while thrid_number_ip < 256:
+        licznik_podsieci += 1
+
+        cN = last_number_ip
+        cH1 = cN + 1
+
+        #bN_first - третя цифра до обчислень
+        bN_first = thrid_number_ip
+
+        #перевіряємо чи степінь більша 256 бо інакше до останньої цифри додасть 512 чи 1024 ...  - (вона вийде за межі 255)
+        if pow2 > 256:
+            step = pow2/256
+            thrid_number_ip += int(step)
+            last_number_ip = last_number_ip + 256
+
+        else:
+            last_number_ip = last_number_ip + pow2
+
+        #bN_last - третя цифра після обчислень
+        if thrid_number_ip >= 1:
+            bN_last = thrid_number_ip-1
+        else:
+            bN_last = thrid_number_ip
+
+        cH_ost = last_number_ip - 2
+        cR = last_number_ip - 1
+
+    #вывожу в консоль все подсети + последний ОТДЕЛЬНО с эмодзи
+        if last_number_ip and thrid_number_ip >= 256:
+            if not last_subnet_printed:
+                print(f"❗ ost. {licznik_podsieci}p:")
+                last_subnet_printed = True
+            print(f"ostH: {bN_last}.{cH_ost}; R:{bN_last}.{cR};")
+        else:
+            print(f"{licznik_podsieci}p:")
+            print(f"ostH: {bN_last}.{cH_ost}; R:{bN_last}.{cR};")
+
+
+
+        #обнуляємо останню цифру ip
+        if pow2 > 256:
+            last_number_ip = 0
+
+        if last_number_ip >= 256:
+            last_number_ip = 0
+            thrid_number_ip += 1
 
 
 '''
@@ -105,19 +181,24 @@ ip_class = get_ip_class(ip_address)
 print(f"\nIP-адреса належить до класи {ip_class}")
 
 
-
+var = True  #варифікація
 #Вибір відповідної функції
 if ip_class =='C':
     print("Маска за замовчуванням 255.255.255.0\n")
     #Перевіряємо
-    validate_host_C(host_count)
+    var = validate_host_C(host_count)
     # Обчислити класу С
-    find_c(ip_address, 2**power_of_two)
+    if var == True:
+        find_c(ip_address, 2**power_of_two)
 
 elif ip_class == 'B':
     print("Маска за замовчуванням 255.255.0.0")
-    # Обчислити класу B
-    pass
+    # Перевіряємо
+    var = validate_host_B(host_count)
+    # Обчислити класу С
+    if var == True:
+        find_B(ip_address, 2 ** power_of_two)
+
 elif ip_class == 'A':
     print("Маска за замовчуванням 255.0.0.0")
     # Обчислити класу A
