4936128626e98157d37d70825aca142c1f914299
AndriiKendzor <kendzorkv@gmail.com>
2024-02-26 17:34:20 +0100

    *

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def index():
     data = {'message': ''}
     return render_template('index.html', data=data)
 
+
+@app.route('/WhoWeAre')
+def who_we_are():
+    data = {'message': ''}
+    return render_template('WhoWeAre.html', data=data)
+
 @app.route('/Contacts_Us')
 def Contacts_us():
     data = {'message': ''}
@@ -343,7 +349,7 @@ def process_ip():
                 if var == True:
                     result = find_c(ip_address, 2 ** power_of_two)
                 else:
-                    result = "Incorrect number of hosts for class C"
+                    result = "Incorrect number of Hosts/Subnets for class C"
 
             elif ip_class == 'B':
                 # Перевіряємо
@@ -352,7 +358,7 @@ def process_ip():
                 if var == True:
                     result = find_B(ip_address, 2 ** power_of_two)
                 else:
-                    result = "Incorrect number of hosts for class B"
+                    result = "Incorrect number of Hosts/Subnets for class B"
 
             elif ip_class == 'A':
                 # Перевіпряємо
@@ -361,7 +367,7 @@ def process_ip():
                 if var == True:
                     result = find_A(ip_address, 2 ** power_of_two)
                 else:
-                    result = "Incorrect number of hosts for class A"
+                    result = "Incorrect number of Hosts/Subnets for class A"
 
             elif ip_class == 'D':
                 result = ("This IP belongs to the reserved class D")
@@ -386,6 +392,14 @@ def process_another_ip():
     result = "________"
     # Приймаємо ip
     ip_address = data.get('ipAddress')
+    # Проверяем, содержит ли IP-адрес буквы
+    if re.search('[a-zA-Z]', ip_address):
+        return "Ошибка: IP-адрес не должен содержать буквы"
+    else:
+        # Если IP-адрес не содержит букв, выполняем остальные действия
+        print("IP-адрес не содержит букв:", ip_address)
+
+
 
     # Приймаємо host
     try:
